ae15c458fe74ea8aff7f51859d6e697d26366d96
Amaury Soto <amaurysotog@gmail.com>
2020-10-08 15:03:22 -0500

    Import Dynamic

--- modulos/04/js/icecream/icecream-machine.js
+++ modulos/04/js/icecream/icecream-machine.js
@@ -3,9 +3,12 @@ import { isRequired } from "./validation.js"
 
 function makeIceCream(flavor1 = isRequired("flavor1"), flavor2 = isRequired("flavor2")) {
   // console.log(flavor1)
+
   alert(`tienes un rico helado de ${flavor1.dataset.flavor} y ${flavor2.dataset.flavor}`)
 }
 
 export {
   makeIceCream
 }
+
+
