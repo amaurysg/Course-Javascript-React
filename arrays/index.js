326aed60f489691c32790e6a668d9728a0f04b55
Amaury Soto <amaurysotog@gmail.com>
2020-10-08 17:02:52 -0500

    Array Concepts

---
+++ arrays/index.js
@@ -0,0 +1,19 @@
+const frutas = ["Apple", "Orange", "Lemon"]
+//UNSHIFT Añade elemento al inicio
+/* frutas.unshift("Naranja") */
+
+//PUSH Añade elemento al final
+/* frutas.push("Uva") */
+
+//POP Elimina el ultimo elemento 
+/* frutas.pop() */
+
+//SHIFT Elimina primer elemento
+/* frutas.shift() */
+
+//indexOf averigua el indice del elemento
+
+/* frutas.reverse() */ //Devuelve valors en reverse
+frutas.values()
+
+console.log(frutas)
